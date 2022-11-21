47ae55013cf071ef0d05318b39c026aa82b756b0
Pablo Torres <pablotorreeifel@gmail.com>
2022-11-20 21:08:45 -0300

    Created project

---
+++ notes-api-java/src/main/java/com/torres/notesapi/NotesApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.torres.notesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApiJavaApplication.class, args);
+	}
+
+}
---
+++ notes-api-java/src/test/java/com/torres/notesapi/NotesApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torres.notesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
