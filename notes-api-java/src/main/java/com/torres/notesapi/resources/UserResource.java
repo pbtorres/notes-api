b4cd36314e39af248e2e5859f6693c824cdbb4b5
Pablo Torres <pablotorreeifel@gmail.com>
2022-11-25 15:24:03 -0300

    Search user by id, created ObjectNotFoundException, StandardError, ResourceExceprionHandler class

--- notes-api-java/src/main/java/com/torres/notesapi/resources/UserResource.java
+++ notes-api-java/src/main/java/com/torres/notesapi/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		//List<User> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findBid(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ notes-api-java/src/main/java/com/torres/notesapi/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.torres.notesapi.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.torres.notesapi.services.exception.ObjectNotFoundException;
+
+@RestControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Not found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
